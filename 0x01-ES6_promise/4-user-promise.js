acda52a331855f3b586fa596ebe3f582669e27c4
Same Titan <49342907+loopopski@users.noreply.github.com>
2023-02-15 16:58:46 +0000

    Simple promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,3 @@
-export default function uploadPhoto(filename) {
-  return Promise.reject(Error(`${filename} cannot be processed`));
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName, lastName });
 }
