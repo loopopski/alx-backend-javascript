02f7d223d050ab809c20cd707a30d96fd0c24436
Zam Titan <49342907+loopopski@users.noreply.github.com>
2023-02-19 09:08:49 -0800

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,16 +1,9 @@
-export default function groceriesList() {
-  const arr = [
-    ['Apples', 10],
-    ['Tomatoes', 10],
-    ['Pasta', 1],
-    ['Rice', 1],
-    ['Banana', 5],
-  ];
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
 
-  const mp = new Map();
-  // eslint-disable-next-line no-unused-vars
-  for (const el of arr) {
-    mp.set(el[0], el[1]);
-  }
-  return mp;
-}
+  map.forEach((value, key) => {
+    if (value === 1) map.set(key, 100);
+  });
+};
+
+export default updateUniqueItems;
