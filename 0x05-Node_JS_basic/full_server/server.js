4539e8e508bfecb8439be990115673620aed44ab
Ibukun Obideyi <49342907+loopopski@users.noreply.github.com>
2023-03-21 05:13:32 -0700

    Organize a complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,14 +1,10 @@
-const express = require('express');
+import app from './routes/index';
 
-const router = require('./routes/index');
+const HOSTNAME = '127.0.0.1';
+const PORT = 1245;
 
-const app = express();
-const port = 1245;
-
-app.use('/', router);
-app.use('/students', router);
-app.use('/students/:major', router);
-
-app.listen(port);
+app.listen(HOSTNAME, PORT, () => {
+  console.log(`Example app running at http://${HOSTNAME}:${PORT}/`);
+});
 
 export default app;
