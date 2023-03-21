5ce77a1711a25c7f798745473ec6f35a43d58158
Ibukun Obideyi <49342907+loopopski@users.noreply.github.com>
2023-03-21 05:08:03 -0700

    Update server.js

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,10 +1,14 @@
-import app from './routes/index';
+const express = require('express');
 
-const HOSTNAME = '127.0.0.1';
-const PORT = 1245;
+const router = require('./routes/index');
 
-app.listen(HOSTNAME, PORT, () => {
-  console.log(`Example app running at http://${HOSTNAME}:${PORT}/`);
-});
+const app = express();
+const port = 1245;
+
+app.use('/', router);
+app.use('/students', router);
+app.use('/students/:major', router);
+
+app.listen(port);
 
 export default app;
