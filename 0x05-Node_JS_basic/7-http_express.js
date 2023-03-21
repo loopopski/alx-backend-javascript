f1b6b39a334245d5b437b6ccdc512d8b41417888
Zam Titan <49342907+loopopski@users.noreply.github.com>
2023-03-21 02:53:21 -0700

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,39 +1,70 @@
 const express = require('express');
-const { argv } = require('process');
 const fs = require('fs');
 
 const app = express();
+const port = 1245;
+const hostname = '127.0.0.1';
+
+const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
+
+function countStudents(path) {
+  const res = [];
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n');
+        const students = lines.slice(1, -1);
+        const numberOfStudents = students.length;
+        const obj = {};
+
+        students.forEach((student) => {
+          const data = student.split(',');
+          const field = data[data.length - 1];
+
+          if (!obj[field]) {
+            obj[field] = {};
+            obj[field].firstname = [];
+            obj[field].count = 1;
+          } else {
+            obj[field].count += 1;
+          }
+          obj[field].firstname.push(data[0]);
+        });
+
+        res.push(`Number of students: ${numberOfStudents}`);
+
+        for (const [field, value] of Object.entries(obj)) {
+          const firstnames = value.firstname.join(', ');
+          res.push(
+            `Number of students in ${field}: ${value.count}. List: ${firstnames}`,
+          );
+        }
+        resolve(res.join('\n'));
+      }
+    });
+  });
+}
 
 app.get('/', (req, res) => {
-  res.set('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
-  fs.readFile(argv[2], 'utf8', (err, data) => {
-    if (err) {
-      throw Error('Cannot load the database');
-    }
-    const result = [];
-    data.split('\n').forEach((data) => {
-      result.push(data.split(','));
+  const studentReport = [];
+  studentReport.push('This is the list of our students');
+  countStudents(DB_FILE)
+    .then((data) => {
+      studentReport.push(data);
+      res.send(studentReport.join('\n'));
+    })
+    .catch((err) => {
+      studentReport.push(err instanceof Error ? err.message : err.toString());
+      res.send(studentReport.join('\n'));
     });
-    result.shift();
-    const newis = [];
-    result.forEach((data) => newis.push([data[0], data[3]]));
-    const fields = new Set();
-    newis.forEach((item) => fields.add(item[1]));
-    const final = {};
-    fields.forEach((data) => { (final[data] = 0); });
-    newis.forEach((data) => { (final[data[1]] += 1); });
-    res.write(`Number of students: ${result.filter((check) => check.length > 3).length}\n`);
-    Object.keys(final).forEach((data) => res.write(`Number of students in ${data}: ${final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}\n`));
-    res.end();
-  });
 });
 
-app.listen(1245);
-
-module.exports = app;
+module.exports = app.listen(port, hostname, () => {
+  console.log(`Example app running at http://${hostname}:${port}/`);
+});
