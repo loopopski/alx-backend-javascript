9e7190d13d8793426d88037df079e37ba55ce050
Zam Titan <49342907+loopopski@users.noreply.github.com>
2023-03-21 03:10:46 -0700

    Application controller

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,1 +1,9 @@
+// App controller
+class AppController {
+  static getHomepage(request, response) {
+    response.status = 200;
+    response.send('Hello Holberton School!');
+  }
+}
 
+export default AppController;
