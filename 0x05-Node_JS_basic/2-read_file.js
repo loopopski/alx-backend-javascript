c71f5b51d2793c52d6e9666108b3b386c6d610ce
Ibukun Obideyi <49342907+loopopski@users.noreply.github.com>
2023-09-29 09:09:15 -0700

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,22 +3,34 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
+    const dataArray = data.toString().split('\n').map((e) => e.trim())
+      .map((e) => e.split(',').map((e) => e.trim()));
+    const dataKeys = dataArray.shift();
     const result = [];
-    data.split('\n').forEach((data) => {
-      result.push(data.split(','));
+    const dataSet = new Set();
+
+    for (let i = 0; i < dataArray.length; i += 1) {
+      const dataJson = {};
+      for (let j = 0; j < dataArray[i].length; j += 1) {
+        if (dataArray[i][j] !== '') {
+          dataJson[dataKeys[j]] = dataArray[i][j];
+          if (dataKeys[j] === 'field') {
+            dataSet.add(dataArray[i][j]);
+          }
+        }
+      }
+      result.push(dataJson);
+    }
+
+    const validResult = result.filter((item) => Object.keys(item).length !== 0);
+    console.log(`Number of students: ${validResult.length}`);
+    dataSet.forEach((value) => {
+      const arr = validResult.filter((item) => item.field === value);
+      const firstNames = arr.map((item) => item.firstname);
+      console.log(`Number of students in ${value}: ${arr.length}. List: ${firstNames.join(', ')}`);
     });
-    result.shift();
-    const newis = [];
-    result.forEach((data) => newis.push([data[0], data[3]]));
-    const fields = new Set();
-    newis.forEach((item) => fields.add(item[1]));
-    const final = {};
-    fields.forEach((data) => { (final[data] = 0); });
-    newis.forEach((data) => { (final[data[1]] += 1); });
-    console.log(`Number of students: ${result.filter((check) => check.length > 3).length}`);
-    Object.keys(final).forEach((data) => console.log(`Number of students in ${data}: ${final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}`));
-  } catch (E) {
-    throw Error('Cannot load the database');
+  } catch (err) {
+    throw new Error('Cannot load the database');
   }
 }
 
