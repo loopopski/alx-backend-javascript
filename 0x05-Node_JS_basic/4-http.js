34fc66f2e553ded97ba9844eee1fa1a97a4c179f
Ibukun Obideyi <49342907+loopopski@users.noreply.github.com>
2023-09-29 09:14:52 -0700

    Task 4 solution

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = 'localhost';
+const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
@@ -9,8 +9,6 @@ const app = http.createServer((req, res) => {
   res.end('Hello Holberton School!');
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, hostname);
 
 module.exports = app;
