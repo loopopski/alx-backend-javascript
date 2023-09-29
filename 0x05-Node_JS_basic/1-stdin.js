0f49f5c9d153f0e31dd307c27315a53c45dcada4
Ibukun Obideyi <49342907+loopopski@users.noreply.github.com>
2023-09-29 09:04:07 -0700

    Task 1 solution

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,14 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
   }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  // process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
